248a5b0bdf7bea204a814fc42286c055bcdfe3c3
LeventSarioglu <leventsarioglu02@gmail.com>
2022-11-16 13:55:49 +0300

    new-assignment

--- AIN1001-theorical/100-square-game.py
+++ AIN1001-theorical/100-square-game.py
@@ -1,4 +1,4 @@
-import random
+from random import randint
 
 squares = [
 {
@@ -18,5 +18,45 @@ players_current_squares = [0, 0]
 
 while players_current_squares[0] < 100 and players_current_squares[1] < 100:
 # TODO: implement game logic
+    tmp = input("Player 1 please press enter.")
+    dice1 = randint(1,6)
+    players_current_squares[0] = players_current_squares[0] + dice1
+
+    print(f"Player 1 rolls {dice1}!")
+    
+    if players_current_squares[0] == squares[0]["penalty"] or players_current_squares[0] == squares[1]["penalty"]:
+        players_current_squares[0] = players_current_squares[0] - 10
+        print(f"Player 1 hit the penalty square and went 10 squares back")
+    
+    if players_current_squares[0] == squares[0]["reward"] or players_current_squares[0] == squares[1]["reward"]:
+        players_current_squares[0] = players_current_squares[0] + 10
+        print(f"Player 1 hit the reward square and went 10 squares forward")
+
+    if players_current_squares[0] < 100:
+        print(f"Player 1 is on {players_current_squares[0]} now!\n")
+    
+    else:
+        print("Player 1 won!")
+        break
 
 
+    tmp = input("Player 2 please press enter.")
+    dice2 = randint(1,6)
+    players_current_squares[1] = players_current_squares[1] + dice2
+
+    print(f"Player 2 rolls {dice2}!")
+    
+    if players_current_squares[1] == squares[0]["penalty"] or players_current_squares[1] == squares[1]["penalty"]:
+        players_current_squares[1] = players_current_squares[1] - 10
+        print(f"Player 2 hit the penalty square and went 10 squares back")
+    
+    if players_current_squares[1] == squares[0]["reward"] or players_current_squares[1] == squares[1]["reward"]:
+        players_current_squares[1] = players_current_squares[1] + 10
+        print(f"Player 2 hit the reward square and went 10 squares forward")
+
+    if players_current_squares[1] < 100:
+        print(f"Player 2 is on {players_current_squares[1]} now!\n")
+    
+    else:
+        print("Player 2 won!")
+        break
