0fef0076ed3cd0fdd547ed3c490e86a6d1f5b238
Levent <leventsarioglu02@gmail.com>
2023-04-15 16:13:03 +0300

    trying to sum numbers individually

--- AIN1002-theorical/amicable-numbers.cpp
+++ AIN1002-theorical/amicable-numbers.cpp
@@ -3,8 +3,32 @@
 using namespace std;
 
 int main() {
-    // Your code goes here
-    int a, b;
+
+    int b, d;
+
+    for (int a = 1100; a < 1300; a++)
+    {
+        int suma = 0;
+        int sumb = 0;
+
+        for (i = 0; i <= a/2; i++)
+        {
+            if (a % i == 0)
+            {
+                suma = suma + i;
+            
+            }
+            
+        }
+
+
+        if (suma == sumb)
+        {
+            cout << a << "and" << b << "are amicable numbers" << endl;
+            break;
+        }
+    }
     
+
     return 0;
 }
